9e699f8c11418038bf500650d75f70877c0c11eb
kimmerss <43441837+kimmerss@users.noreply.github.com>
2018-12-19 20:25:23 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+$("#selfexposus").click(function(){
+    $("#chimp").slideToggle();
+});
+
+$("#woodappear").click(function(){
+    $("#writing").fadeToggle();
+});
+
+$("#unexpectomous").click(function(){
+    $("#weird").toggle();
+});
+
+});
+
+
+
